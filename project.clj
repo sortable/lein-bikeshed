e54354e8035950c67b4eaf463c67cf82280e5559
Mark Feeney <mark.feeney@gmail.com>
2017-02-01 13:50:48 -0500

    Sortableize

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject lein-bikeshed "0.4.2-SNAPSHOT"
+(defproject lein-bikeshed-sortable "0.4.2-SNAPSHOT"
   :description (str "A Leiningen plugin designed to tell you your code is bad, "
                     "and that you should feel bad")
-  :url "https://github.com/dakrone/lein-bikeshed"
+  :url "https://github.com/sortable/lein-bikeshed"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in :leiningen
--- src/leiningen/bikeshed.clj
+++ src/leiningen/bikeshed.clj
@@ -37,7 +37,7 @@
        (-> project
            (update-in [:dependencies]
                       conj
-                      ['lein-bikeshed "0.4.2-SNAPSHOT"]))
+                      ['lein-bikeshed-sortable "0.4.2-SNAPSHOT"]))
        `(if (bikeshed.core/bikeshed
              '~project
              {:max-line-length (or (:max-line-length ~opts)
